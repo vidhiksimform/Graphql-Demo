7b85d1839b1e98992644e24056f562faf0105742
vidhiksimform <vidhi.k@simformsolutions.com>
2023-12-25 12:15:48 +0530

    graphql-frontend

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Header from "./components/Header";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProjectDetail from "./pages/ProjectDetail";
+import "./index.css";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<ProjectDetail />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
